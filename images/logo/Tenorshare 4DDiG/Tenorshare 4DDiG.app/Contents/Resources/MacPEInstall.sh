b27539017efe18cf0296d362786be9919f22eeff
강동우 <ghkjiop92@naver.com>
2024-11-10 20:51:10 +0900

    작업사항 커밋

--- images/logo/Tenorshare 4DDiG/Tenorshare 4DDiG.app/Contents/Resources/MacPEInstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#  Script.sh
-#  AnyDataRecovery
-#
-#  Created by tenorshare on 2019/11/16.
-#  Copyright © 2019 Tenorshare. All rights reserved.
-
-# 在这个脚本里面存放制作启动盘的命令
--- images/logo/Tenorshare 4DDiG/Tenorshare 4DDiG.app/Contents/Resources/UltFone Mac Data Recovery.app/Contents/Resources/copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#  copy.sh
-#  UltData
-#
-#  Created by tenorshare on 2019/11/28.
-#  Copyright © 2019 Tenorshare. All rights reserved.
