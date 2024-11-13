67c79ae455f0feb90b5e577ac15900c10f5cfd23
Tokyun02 <ehrbsghkrk@naver.com>
2024-11-13 17:39:00 +0900

    chore : 버튼 초기 비활성화

--- account.js
+++ account.js
@@ -1,11 +1,16 @@
 const authForm = document.querySelector(".auth-form");
 
 const validCheck = {
-  emailCheck: () => {},
-  nicknameCheck: () => {},
-  passwordCheck: () => {},
-  passwordEqualCheck: () => {},
+  email: () => {},
+  nickname: () => {},
+  password: () => {},
+  "password-confirm": () => {},
 };
 
-authForm.addEventListener("focusout", ({ target }) => {});
+//input의 유효성 검사
+authForm.addEventListener("focusout", ({ target }) => {
+  validCheck[target.name]();
+});
+
+//form의 유효성 검사, 눈 모양 아이콘 클릭 이벤트
 authForm.addEventListener("click", ({ target }) => {});
