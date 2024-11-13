aa0787a531a2c9788525c34899844ffdb966c9cf
Tokyun02 <ehrbsghkrk@naver.com>
2024-11-13 17:12:48 +0900

    chore : 유효성 검사 및 이벤트 등록 포맷 작성

---
+++ account.js
@@ -0,0 +1,11 @@
+const authForm = document.querySelector(".auth-form");
+
+const validCheck = {
+  emailCheck: () => {},
+  nicknameCheck: () => {},
+  passwordCheck: () => {},
+  passwordEqualCheck: () => {},
+};
+
+authForm.addEventListener("focusout", ({ target }) => {});
+authForm.addEventListener("click", ({ target }) => {});
