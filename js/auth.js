4da715fb37f94f65a80030f3d48fc072a8048b97
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-11-14 17:24:25 +0900

    ✨ feat: 회원가입 페이지 유효성 검사 기능 추가

--- js/auth.js
+++ js/auth.js
@@ -3,12 +3,29 @@ const emailError = document.querySelector('.emailInput.inputError');
 const passwordError = document.querySelector('.passwordInput.inputError');
 const password = document.querySelector('.passwordInput');
 const loginBtn = document.querySelector('.login.button');
+const nickName = document.querySelector('.nickNameInput');
+const nameError = document.querySelector('.nickNameInput.inputError');
+const confirmPw = document.querySelector('.confirmPassword');
+const confirmPwError = document.querySelector('.confirmPassword.inputError');
+const signupBtn = document.querySelector('.signup.button');
 
 function updateLoginButtonState() {
-  if (!email.value || !password.value || email.classList.contains('invalid') || password.classList.contains('invalid')) {
-    loginBtn.disabled = true;
-  } else {
-    loginBtn.disabled = false;
+  if (loginBtn) {
+    if (!email.value || !password.value || email.classList.contains('invalid') || password.classList.contains('invalid')) {
+      loginBtn.disabled = true;
+    } else {
+      loginBtn.disabled = false;
+    }
+  }
+}
+
+function updateSignupButtonState() {
+  if (signupBtn) {
+    if (!email.value || !password.value || !nickName.value || !confirmPw.value || email.classList.contains('invalid') || password.classList.contains('invalid') || nickName.classList.contains('invalid') || confirmPw.classList.contains('invalid')) {
+      signupBtn.disabled = true;
+    } else {
+      signupBtn.disabled = false;
+    }
   }
 }
 
@@ -24,6 +41,7 @@ email.addEventListener('blur', () => {
     emailError.textContent = '';
   }
   updateLoginButtonState();
+  updateSignupButtonState();
 })
 
 password.addEventListener('blur', () => {
@@ -38,15 +56,47 @@ password.addEventListener('blur', () => {
     passwordError.textContent = '';
   }
   updateLoginButtonState();
+  updateSignupButtonState();
 })
 
 email.addEventListener('input', updateLoginButtonState);
 password.addEventListener('input', updateLoginButtonState);
 
 updateLoginButtonState();
+updateSignupButtonState();
+
+if (nickName && nameError) {
+  nickName.addEventListener('blur', () => {
+    if (nickName.value === '') {
+      nickName.classList.add('invalid');
+      nameError.textContent = '닉네임을 입력해주세요';
+    } else {
+      nickName.classList.remove('invalid');
+      nameError.textContent = '';
+    }
+    updateSignupButtonState();
+  })
+}
+
+confirmPw.addEventListener('blur', () => {
+  if (confirmPw.value !== password.value) {
+    confirmPw.classList.add('invalid');
+    confirmPwError.textContent = '비밀번호가 일치하지 않습니다.';
+  } else {
+    confirmPw.classList.remove('invalid');
+    confirmPwError.textContent = '';
+  }
+  updateSignupButtonState();
+})
 
 loginBtn.addEventListener('click', (e) => {
   // 버튼 타입 submit 때문에 일단 막아놓음
   e.preventDefault();
   window.location.href = '/items';
 })
+
+signupBtn.addEventListener('click', (e) => {
+  console.log('Signup button clicked');
+  e.preventDefault();
+  window.location.href = '/signin';
+});
