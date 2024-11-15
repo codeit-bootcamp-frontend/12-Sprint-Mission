7a3e357dc18a403b207dc08e3ab2350adc75522d
Tokyun02 <ehrbsghkrk@naver.com>
2024-11-15 11:57:34 +0900

    refactor : 유효성 검사의 통과 return 타입 true로 수정 및 removeError함수 리팩토링

--- scripts/account.js
+++ scripts/account.js
@@ -6,8 +6,8 @@ const addErrorMessage = (target, errorMessage) => {
   const nextNode = target.nextElementSibling;
 
   if (!errorMessage) return;
-  target.classList.add("error-input-border");
 
+  target.classList.add("error-input-border");
   if (!nextNode || nextNode.classList.contains("toggle-password")) {
     const errorElement = document.createElement("span");
     errorElement.textContent = errorMessage;
@@ -22,7 +22,8 @@ const addErrorMessage = (target, errorMessage) => {
 
 const removeErrorMessage = (target) => {
   const nextNode = target.nextElementSibling;
-  if (nextNode.classList.contains("error-message")) nextNode.remove();
+  if (nextNode && nextNode.classList.contains("error-message"))
+    nextNode.remove();
   target.classList.remove("error-input-border");
 };
 
@@ -35,6 +36,23 @@ const changePasswordType = () => {
   document.querySelectorAll(".password-field").forEach((element) => {
     element.type = "password";
   });
+};
+
+const activatedButton = (validCheckResults) => {
+  const submitButton = document.querySelector(".submitButton");
+  validCheckResults.every((value) => value === true)
+    ? (submitButton.disabled = false)
+    : (submitButton.disabled = true);
+};
+
+const validCheckForm = () => {
+  return Object.entries(validCheckInput).map(
+    ([validInputName, validCheckFunc]) => {
+      const inputElement = document.querySelector(`#${validInputName}`);
+      if (inputElement === null) return true;
+      return validCheckFunc(inputElement);
+    }
+  );
 };
 
 const togglePassword = (target) => {
@@ -50,52 +68,44 @@ const togglePassword = (target) => {
   }
 };
 
-const validCheck = {
+const validCheckInput = {
   email: (target) => {
     const emailRegex = VAILD_REGEX.EMAIL_REGEX;
     if (target.value.length === 0) return ERROR_MESSAGE.NOT_INPUT_EMAIL;
     if (!emailRegex.test(target.value)) return ERROR_MESSAGE.INVALID_EMAIL;
-    if (target.nextElementSibling) removeErrorMessage(target);
-    return null;
+    removeErrorMessage(target);
+    return true;
   },
   nickname: (target) => {
     if (target.value.length === 0) return ERROR_MESSAGE.NOT_INPUT_NICKNAME;
-    if (target.nextElementSibling) removeErrorMessage(target);
-    return null;
+    removeErrorMessage(target);
+    return true;
   },
   password: (target) => {
     if (target.value.length === 0) return ERROR_MESSAGE.NOT_INPUT_PASSWORD;
     if (target.value.length < 8) return ERROR_MESSAGE.MORE_THEN_8DIGIT;
+    //비밀번호 확인을 기준으로 비밀번호 란을 채울 경우 고려
     if (document.querySelector("#password-confirm"))
-      validCheck["password-confirm"](
+      validCheckInput["password-confirm"](
         document.querySelector("#password-confirm")
       );
-    if (target.nextElementSibling) removeErrorMessage(target);
-    return null;
+    removeErrorMessage(target);
+    return true;
   },
   "password-confirm": (target) => {
     if (target.value.length === 0) return ERROR_MESSAGE.NOT_INPUT_PASSWORD;
     if (target.value.length < 8) return ERROR_MESSAGE.MORE_THEN_8DIGIT;
     if (!comparePassword(target)) return ERROR_MESSAGE.NOT_MATCH_PASSWORD;
-    if (target.nextElementSibling) removeErrorMessage(target);
-    return null;
+    removeErrorMessage(target);
+    return true;
   },
 };
 
 const focusoutHandler = ({ target }) => {
-  if (validCheck[target.name]) {
-    addErrorMessage(target, validCheck[target.name](target));
-  }
-
-  const result = Object.values(validCheck).map((validFunc) => {
-    const element = document.querySelector(`#${validFunc.name}`);
-    if (element === null) return null;
-    return validFunc(element);
-  });
-
-  if (result.every((value) => value === null)) {
-    const submitButton = document.querySelector(".submitButton");
-    submitButton.disabled = false;
+  if (validCheckInput[target.name]) {
+    addErrorMessage(target, validCheckInput[target.name](target));
+    const result = validCheckForm();
+    activatedButton(result);
   }
 };
 
