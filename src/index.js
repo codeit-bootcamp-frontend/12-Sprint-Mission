ba91c38d701c4f544fc3ba7e871f78aefc7d71ac
chanki.kim <chanki.kim89@gmail.com>
2024-11-16 10:55:17 +0900

    feat: 라우팅 설정 및 임시페이지 연결

--- src/index.js
+++ src/index.js
@@ -1,11 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
 import App from "./App";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Privacy from "./pages/Privacy";
+import Faq from "./pages/Faq";
+import Items from "./pages/Items";
+import Boards from "./pages/Boards";
 import "./assets/scss/style.scss";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        element: <Layout hasFooter />,
+        children: [
+          {
+            path: "",
+            element: <Landing />,
+          },
+        ],
+      },
+      {
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "signup",
+            element: <Signup />,
+          },
+          {
+            path: "privacy",
+            element: <Privacy />,
+          },
+          {
+            path: "faq",
+            element: <Faq />,
+          },
+        ],
+      },
+      {
+        element: <Layout hasNav />,
+        children: [
+          {
+            path: "items",
+            element: <Items />,
+          },
+          {
+            path: "boards",
+            element: <Boards />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
