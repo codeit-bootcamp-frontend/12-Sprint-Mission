e5b4e0a8bf3c42c38b828effe05afc5a9196ebef
chanki.kim <chanki.kim89@gmail.com>
2024-11-16 14:06:40 +0900

    feat: Landing 페이지 작업

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,3 +1,14 @@
+import Banner from "./Banner";
+import Feature from "./Feature";
+import { heroBannerData, footerBannerData } from "./BannerContent";
+import { featureList } from "./featureContent";
+
 export default function Landing() {
-  return <>랜딩페이지</>;
+  return (
+    <>
+      <Banner {...heroBannerData} />
+      <Feature list={featureList} />
+      <Banner {...footerBannerData} />
+    </>
+  );
 }
