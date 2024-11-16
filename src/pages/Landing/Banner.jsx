614f9ffc318c088b6f7e247ba16a69ec21a776c8
chanki.kim <chanki.kim89@gmail.com>
2024-11-16 14:06:09 +0900

    feat: Landing - Banner 컴포넌트 작업

---
+++ src/pages/Landing/Banner.jsx
@@ -0,0 +1,29 @@
+import Button from "../../components/Button";
+import styles from "./Banner.module.scss";
+
+export default function Banner({
+  title,
+  bannerType = "hero",
+  action,
+  bannerImg,
+}) {
+  return (
+    <section className={`${styles.banner} ${styles[`${bannerType}-banner`]}`}>
+      <div className={styles.inner}>
+        <div className={styles["banner-content"]}>
+          <h2 className={styles["banner-title"]}>{title}</h2>
+          {action && (
+            <Button to={action.to} size="lg">
+              {action.label}
+            </Button>
+          )}
+        </div>
+        <img
+          className={styles["banner-img"]}
+          src={bannerImg.src}
+          alt={bannerImg.alt}
+        />
+      </div>
+    </section>
+  );
+}
