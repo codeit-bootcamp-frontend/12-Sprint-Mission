c37fc4e46a2b98123a4488cdf4ec4f15492577b3
chanki.kim <chanki.kim89@gmail.com>
2024-11-17 23:04:55 +0900

    feat: 비밀번호 인풋에 가리기/보이기 아이콘 기능 작업

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,3 +1,6 @@
+import { useRef, useState } from "react";
+import iconViewOn from "../../assets/img/icon/icon_view_on.svg";
+import iconViewOff from "../../assets/img/icon/icon_view_off.svg";
 import styles from "./styles.module.scss";
 
 export default function Input({
@@ -8,7 +11,13 @@ export default function Input({
   value,
   ...props
 }) {
+  const [currentType, setCurrentType] = useState(type);
   const valid = value && !error;
+
+  function handleVisibility() {
+    setCurrentType((prev) => (prev === "password" ? "text" : "password"));
+  }
+
   return (
     <div className={styles["form-item"]}>
       <label className={styles["item-label"]} htmlFor={id}>
@@ -16,11 +25,27 @@ export default function Input({
       </label>
       <div className={styles["item-field"]}>
         <input
-          type={type}
+          type={currentType}
           className={`${styles["input-box"]} ${valid ? styles.valid : ""}`}
           value={value}
           {...props}
         />
+        {type === "password" && (
+          <button
+            type="button"
+            className={`${styles["item-btn"]} ${styles["visibility-btn"]}`}
+            onClick={handleVisibility}
+          >
+            <img
+              src={currentType === "password" ? iconViewOff : iconViewOn}
+              alt={
+                currentType === "password"
+                  ? "비밀번호 표시하기"
+                  : "비밀번호 가리기"
+              }
+            />
+          </button>
+        )}
       </div>
       {error && <div className={styles["item-error"]}>{error}</div>}
     </div>
