471cdeb1fe7abc3b5b7bfaa838344a2a4960c16f
chanki.kim <chanki.kim89@gmail.com>
2024-11-18 14:31:57 +0900

    refactor: api통신시 사용하는 쪽에서 try,catch를 써서 에러를 핸들링하도록 수정

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,28 +1,24 @@
 const { VITE_API_URL } = import.meta.env;
 
 export async function login({ email, password }) {
-  try {
-    const res = await fetch(`${VITE_API_URL}/auth/signIn`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
+  const res = await fetch(`${VITE_API_URL}/auth/signIn`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || "에러가 발생했습니다.");
-    }
+  const data = await res.json();
 
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw error;
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
   }
+
+  return data;
 }
 
 export async function signUp({
@@ -31,72 +27,58 @@ export async function signUp({
   password,
   passwordConfirmation,
 }) {
-  try {
-    const res = await fetch(`${VITE_API_URL}/auth/signUp`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        nickname,
-        password,
-        passwordConfirmation,
-      }),
-    });
+  const res = await fetch(`${VITE_API_URL}/auth/signUp`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      nickname,
+      password,
+      passwordConfirmation,
+    }),
+  });
 
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || "에러가 발생했습니다.");
-    }
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw error;
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
   }
+
+  return data;
 }
 
 export async function refreshAccessToken(refreshToken) {
-  try {
-    const res = await fetch(`${VITE_API_URL}/auth/refresh-token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        refreshToken,
-      }),
-    });
+  const res = await fetch(`${VITE_API_URL}/auth/refresh-token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      refreshToken,
+    }),
+  });
 
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || "에러가 발생했습니다.");
-    }
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw error;
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
   }
+
+  return data;
 }
 
 export async function getUser(accessToken) {
-  try {
-    const res = await fetch(`${VITE_API_URL}/users/me`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+  const res = await fetch(`${VITE_API_URL}/users/me`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || "에러가 발생했습니다.");
-    }
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw error;
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
   }
+
+  return data;
+  s;
 }
