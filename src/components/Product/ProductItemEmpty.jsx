12128bf0d330a61a08718528d1dadced385eedf4
chanki.kim <chanki.kim89@gmail.com>
2024-11-19 09:45:49 +0900

    feat: 상품이 없는 상태 컴포넌트 작업

---
+++ src/components/Product/ProductItemEmpty.jsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export default function ProductItemEmpty() {
+  return <div className={styles.empty}>상품이 없습니다.</div>;
+}
--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,7 +1,11 @@
 import ProductItem from "./ProductItem";
+import ProductItemEmpty from "./ProductItemEmpty";
 import styles from "./styles.module.scss";
 
 export default function ProductList({ mode = "", items, keyword }) {
+  if (!items.length) {
+    return <ProductItemEmpty />;
+  }
   return (
     <ul className={`${styles.items} ${styles[mode]}`}>
       {items.map((item) => (
