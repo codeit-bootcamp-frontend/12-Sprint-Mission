6ffbdba15125342c17bbce0634d124ffb5a64d06
chanki.kim <chanki.kim89@gmail.com>
2024-11-19 20:02:24 +0900

    refactor: 디바이스타입 리턴함수 수정 (너비를 자신이 체크해서 사용하는 입장에서 호출만 하도록)

--- src/util/breakpoints.js
+++ src/util/breakpoints.js
@@ -1,7 +1,8 @@
 export const BREAKPOINT_TABLET = 1024;
 export const BREAKPOINT_MOBILE = 768;
 
-export function getDeviceType(browserWidth) {
+export function getDeviceType() {
+  const browserWidth = window.innerWidth;
   if (browserWidth < BREAKPOINT_MOBILE) return "mobile";
   if (browserWidth < BREAKPOINT_TABLET) return "tablet";
   return "pc";
