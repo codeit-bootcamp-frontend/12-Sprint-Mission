4b503b4637c902405047ad071ac7f79a1bec04da
chanki.kim <chanki.kim89@gmail.com>
2024-11-19 17:19:26 +0900

    fix: 헤더 로고 컴포넌트 Link컴포넌트로 수정

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logoPC from "../../assets/img/common/logo_full.svg";
 import logoMobile from "../../assets/img/common/logo_text.svg";
 import styles from "./styles.module.scss";
@@ -5,14 +6,14 @@ import styles from "./styles.module.scss";
 export default function Logo() {
   return (
     <h1 className={styles.logo}>
-      <a href="/">
+      <Link to="/">
         <img className={styles["logo-pc"]} src={logoPC} alt="판다마켓" />
         <img
           className={styles["logo-mobile"]}
           src={logoMobile}
           alt="판다마켓"
         />
-      </a>
+      </Link>
     </h1>
   );
 }
