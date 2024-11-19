89e558dc171ae4d0a725e7dabe395bebf1b6259f
chanki.kim <chanki.kim89@gmail.com>
2024-11-19 09:27:24 +0900

    feat: 상품 서비스 로직 작성

---
+++ src/service/product.js
@@ -0,0 +1,32 @@
+const { VITE_API_URL } = import.meta.env;
+
+export async function getProducts({
+  page = 1,
+  pageSize = 10,
+  orderBy = "recent",
+  keyword = "",
+}) {
+  const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
+  const res = await fetch(`${VITE_API_URL}/products?${query}`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
+  }
+
+  return data;
+}
+
+export async function getBestProducts() {
+  const query = `page=1&pageSize=4&orderBy=favorite`;
+  const res = await fetch(`${VITE_API_URL}/products?${query}`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
+  }
+
+  return data;
+}
