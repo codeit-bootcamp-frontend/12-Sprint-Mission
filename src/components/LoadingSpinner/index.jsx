5664966003e4058994ef31bcac86ef7ccf155ae8
chanki.kim <chanki.kim89@gmail.com>
2024-11-21 00:31:51 +0900

    feat: 로딩스피너 개선 - 위치, 색상 프롭을 추가로 받음

--- src/components/LoadingSpinner/index.jsx
+++ src/components/LoadingSpinner/index.jsx
@@ -1,8 +1,15 @@
+import clsx from "clsx";
 import styles from "./styles.module.scss";
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ position = "fixed", light }) {
   return (
-    <div className={styles.container}>
+    <div
+      className={clsx(
+        styles.container,
+        styles[position],
+        light && styles.light
+      )}
+    >
       <div className={styles.loader}></div>
     </div>
   );
