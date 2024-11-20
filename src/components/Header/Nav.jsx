613e650f0ece88358388b4a4c68efe8917b319f9
chanki.kim <chanki.kim89@gmail.com>
2024-11-20 20:06:57 +0900

    refactor: Nav 개선 (활성화상태 개선, clsx 도입) - NavLink의 isActive로 체크하기 힘든 케이스 개선 - clsx도입으로 클래스명 조합 개선

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import clsx from "clsx";
 import styles from "./styles.module.scss";
 
 const MENU = [
@@ -9,10 +10,17 @@ const MENU = [
   {
     path: "/items",
     title: "중고마켓",
+    activePath: ["/items", "/addItem"],
   },
 ];
 
+function isInclude(path, list = []) {
+  return list.includes(path);
+}
+
 export default function Nav() {
+  const { pathname } = useLocation();
+
   return (
     <ul className={styles.gnb}>
       {MENU.map((item) => {
@@ -21,7 +29,11 @@ export default function Nav() {
             <NavLink
               to={item.path}
               className={({ isActive }) =>
-                `${styles["gnb-item"]} ${isActive ? styles.active : ""}`
+                clsx(
+                  styles["gnb-item"],
+                  isActive && styles.active,
+                  isInclude(pathname, item.activePath) && styles.active
+                )
               }
             >
               {item.title}
