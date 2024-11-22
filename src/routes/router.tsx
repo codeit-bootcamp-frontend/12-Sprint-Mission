9f31669c40c5bc1c9ce4de57d6a2adc06516fba4
Tokyun02 <ehrbsghkrk@naver.com>
2024-11-22 20:39:17 +0900

    chore : 라우팅 설정 및 파일 디렉토리 구조화

---
+++ src/routes/router.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from 'react-router-dom';
+import React from 'react';
+import Layout from '../pages/Layout';
+import Home from '../pages/Home/Home';
+import Login from '../pages/Login/Login';
+import Signup from '../pages/Signup/Signup';
+import AddItem from '../pages/AddItem/AddItem';
+import Items from '../pages/Items/Items';
+import Privacy from '../pages/Privacy/Privacy';
+import Faq from '../pages/Faq/Faq';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'login', element: <Login /> },
+      { path: 'signup', element: <Signup /> },
+      { path: 'addItem', element: <AddItem /> },
+      { path: 'items', element: <Items /> },
+      { path: 'privacy', element: <Privacy /> },
+      { path: 'faq', element: <Faq /> },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
+export default router;
