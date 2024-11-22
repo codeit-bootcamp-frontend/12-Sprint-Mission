f29f9d17f2ceb2cdf655ec073f14c82e098bb4bd
LMS10 <ggb7410@naver.com>
2024-11-22 17:42:12 +0900

    feat(component): BestItemsSection 컴포넌트 구현

---
+++ src/pages/MarketPage/MarketPage.jsx
@@ -0,0 +1,12 @@
+import BestItemsSection from "./components/BestItemsSection";
+import "./MarketPage.css";
+
+function MarketPage() {
+  return (
+    <div className="wrapper">
+      <BestItemsSection />
+    </div>
+  );
+}
+
+export default MarketPage;
