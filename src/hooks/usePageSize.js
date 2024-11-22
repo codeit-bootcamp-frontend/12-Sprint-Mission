f3381bf0545324baf0ccbde527789c87e7a9ca39
chanki.kim <chanki.kim89@gmail.com>
2024-11-22 21:24:21 +0900

    refactor: usePageSize 수정 - 초기값 설정 - 불필요한 조건 삭제 (이전에는 숫자를 받을지 객체를 받을지 몰랐기때문에)

--- src/hooks/usePageSize.js
+++ src/hooks/usePageSize.js
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import { getDeviceType } from "@util/breakpoints";
 import { debounce } from "@util/debounce";
 
-export default function usePageSize(initialPageSize) {
-  const isResponse = typeof initialPageSize === "object";
-  const [pageSize, setPageSize] = useState(() =>
-    isResponse ? initialPageSize[getDeviceType()] : initialPageSize
+export default function usePageSize(
+  initialPageSize = {
+    pc: 10,
+    tablet: 6,
+    mobile: 4,
+  }
+) {
+  const [pageSize, setPageSize] = useState(
+    () => initialPageSize[getDeviceType()]
   );
 
   useEffect(() => {
-    if (!isResponse) return;
-
     const debounceHandleResize = debounce(function () {
       const targetSize = initialPageSize[getDeviceType()];
       setPageSize(targetSize);
