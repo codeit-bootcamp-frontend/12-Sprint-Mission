0efe4af47e648ca6251251053acd5e5ed26cebbe
강동우 <ghkjiop92@naver.com>
2024-11-24 03:26:18 +0900

    미션 3 다시 올리기

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
-const inputPush = document.getElementById("inputPush");
+const input = document.querySelector(".inputPush:focus");
 
-if (inputPush) {
-  inputPush.addEventListener("focusout", (event) => {
-    // 포커스를 벗어난 후 배경색을 핑크로 변경
-    event.target.style.background = "pink";
-    console.log("잘못된 이메일입니다!");
-  });
-} else {
-  console.error("inputPush 요소를 찾을 수 없습니다.");
+if (input) {
+  input.onfocus = focus; // 포커스를 받을 때 호출할 함수
+  input.onblur = blur; // 포커스를 벗어날 때 호출할 함수
+}
+
+function focus() {
+  this.style.backgroundColor = "green"; // 포커스를 받으면 배경을 녹색으로 변경
+}
+
+function blur() {
+  this.style.backgroundColor = ""; // 포커스를 벗어나면 배경을 원래 상태로 되돌림
 }
