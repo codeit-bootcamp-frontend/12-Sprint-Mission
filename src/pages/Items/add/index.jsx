a212c92d1e1ee705e0a16051a21ff22bac3ab67c
chanki.kim <chanki.kim89@gmail.com>
2024-11-23 16:30:14 +0900

    fix: github 캐시 제거

--- src/pages/Items/add/index.jsx
+++
@@ -1,5 +0,0 @@
-import Temporary from "@components/Temp";
-
-export default function ItemAdd() {
-  return <Temporary title="아이템 등록페이지" />;
-}
--- src/pages/Items/detail/index.jsx
+++
@@ -1,5 +0,0 @@
-import Temporary from "@components/Temp";
-
-export default function ItemDetail() {
-  return <Temporary title="아이템 상세페이지" />;
-}
