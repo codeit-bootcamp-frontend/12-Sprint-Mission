5756b71ad2fe0e64ff0d7dab863f9d02ef688445
chanki.kim <chanki.kim89@gmail.com>
2024-11-23 22:47:03 +0900

    refactor: Avatar 컴포넌트 개선 - 사용자 프로필 이미지 다운로드 실패시 디폴트 이미지 설정

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -3,9 +3,14 @@ import styles from "./styles.module.scss";
 
 export default function Avatar({ nickname, img }) {
   const avatarImg = img || defaultAvatar;
+
+  function handleError(e) {
+    e.target.src = defaultAvatar;
+  }
+
   return (
     <figure className={styles.avatar}>
-      <img src={avatarImg} alt={nickname} />
+      <img src={avatarImg} alt={nickname} onError={handleError} />
     </figure>
   );
 }
