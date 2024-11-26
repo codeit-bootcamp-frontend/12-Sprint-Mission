5b03f7bf10bc5bddd7e0ef23a9ef41ed339f0ca8
chanki.kim <chanki.kim89@gmail.com>
2024-11-26 13:59:58 +0900

    fix: Proctected Route에서 accesstoken을 검사하도록 변경 - user정보는 로컬스토리지에 저장안하므로 토큰을 검사 - user정보는 새로고침후 새로 가져오기전까지는 계속 null상태임

--- src/pages/ProtectedRoute/index.jsx
+++ src/pages/ProtectedRoute/index.jsx
@@ -3,10 +3,10 @@ import { useAuth } from "@/context/useAuth";
 
 export default function ProtectedRoute({ children }) {
   const {
-    auth: { user },
+    auth: { accessToken },
   } = useAuth();
 
-  if (!user) {
+  if (!accessToken) {
     alert("로그인이 필요합니다.");
     return <Navigate to="/login" replace />;
   }
