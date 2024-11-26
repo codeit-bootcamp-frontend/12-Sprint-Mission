4aa9d2a97215f5c12fec6ecc75f3bc15dba4ebf5
chanki.kim <chanki.kim89@gmail.com>
2024-11-26 10:18:49 +0900

    feat: 이미지 업로드 및 상품추가 api 호출 작업

--- src/service/product.js
+++ src/service/product.js
@@ -28,3 +28,59 @@ export async function getBestProducts({ pageSize }, { signal }) {
 
   return data;
 }
+
+export async function uploadProductImage(formData, accessToken) {
+  const res = await fetch(`${VITE_API_URL}/images/upload`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: formData,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
+  }
+
+  return data;
+}
+
+export async function addProduct(productData, accessToken) {
+  const res = await fetch(`${VITE_API_URL}/products`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(productData),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
+  }
+
+  return data;
+}
+
+export async function deleteProduct(productId, accessToken) {
+  const res = await fetch(`${VITE_API_URL}/products/${productId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "에러가 발생했습니다.");
+  }
+
+  return data;
+}
