6d438e0ca6728bce119abfb811d91291112309be
chanki.kim <chanki.kim89@gmail.com>
2024-11-26 12:10:07 +0900

    chore: 사용안하는 코드 제거

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,9 @@
-import { useAuth } from "@context/useAuth";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import Util from "./Util";
 import styles from "./styles.module.scss";
 
 export default function Header({ showNav = false }) {
-  const {
-    auth: { user },
-    handleLogout,
-  } = useAuth();
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
