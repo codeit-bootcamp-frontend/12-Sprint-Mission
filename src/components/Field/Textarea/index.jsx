0f6a2698e78fb24282375ded7e8d17b21674578b
chanki.kim <chanki.kim89@gmail.com>
2024-11-27 11:37:20 +0900

    chore: Textarea 컴포넌트내 불필요한 코드 정리

--- src/components/Field/Textarea/index.jsx
+++ src/components/Field/Textarea/index.jsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
 import clsx from "clsx";
 import FieldContainer from "../FieldContainer";
 import styles from "../styles.module.scss";
 
-export default function Textarea({
-  type = "text",
-  id,
-  label,
-  error,
-  value,
-  ...props
-}) {
-  const [currentType, setCurrentType] = useState(type);
+export default function Textarea({ id, label, error, value, ...props }) {
   const valid = value && !error;
 
   return (
     <FieldContainer id={id} label={label} error={error}>
       <textarea
-        type={currentType}
         className={clsx(
           styles["textarea-box"],
           valid && styles.valid,
