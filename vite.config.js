19b69a7604275f17818d6a3be1d50614d5652fd5
chanki.kim <chanki.kim89@gmail.com>
2024-11-27 18:44:20 +0900

    chore: alias 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
       {
         find: "@components",
         replacement: path.resolve(__dirname, "src/components"),
