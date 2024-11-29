5b9232b1950504ab8008735bf442653d6c6108bc
LMS10 <ggb7410@naver.com>
2024-11-29 18:32:29 +0900

    feat(page): 상품 등록 페이지(AddItemPage) titleSection 작업

--- src/pages/AddItemPage/AddItemPage.jsx
+++ src/pages/AddItemPage/AddItemPage.jsx
@@ -1,5 +1,18 @@
+import "./AddItemPage.css";
+
 function AddItemPage() {
-  return <div>상품 등록하기</div>;
+  return (
+    <div className="container">
+      <form>
+        <div className="titleSection">
+          <h1 className="sectionTitle">상품 등록하기</h1>
+          <button type="submit" className="addItemButton button">
+            등록
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddItemPage;
