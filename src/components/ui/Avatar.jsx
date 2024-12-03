8d74dcb55a53095f65f2449ea1214fc03798af65
chanki.kim <chanki.kim89@gmail.com>
2024-12-03 13:15:54 +0900

    feat: Avatar 컴포넌트 개선 (hover props추가)

--- src/components/ui/Avatar.jsx
+++ src/components/ui/Avatar.jsx
@@ -1,15 +1,18 @@
+import clsx from "clsx";
 import defaultAvatar from "@assets/img/icon/icon_avatar.svg";
 import styles from "./Avatar.module.scss";
 
-export function Avatar({ nickname, img }) {
+export function Avatar({ nickname, img, hover }) {
   const avatarImg = img || defaultAvatar;
 
   function handleError(e) {
     e.target.src = defaultAvatar;
   }
 
+  const css = clsx(styles.avatar, hover && styles.hover);
+
   return (
-    <figure className={styles.avatar}>
+    <figure className={css}>
       <img src={avatarImg} alt={nickname} onError={handleError} />
     </figure>
   );
