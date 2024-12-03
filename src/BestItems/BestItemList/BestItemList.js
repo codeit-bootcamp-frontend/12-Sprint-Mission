3224bfc325f686f7f7da57da4b1af37c3442936f
박형준 <spfe1245@codeit.com>
2024-12-03 18:55:44 +0900

    Add Best items desktop size

--- src/BestItems/BestItemList/BestItemList.js
+++ src/BestItems/BestItemList/BestItemList.js
@@ -2,12 +2,18 @@ import heart from '../../assets/ic_heart.svg';
 import '../BestItems.css';
 
 function BestItemList({ item, index }) {
+  const price = item.price.toLocaleString();
+
   return (
     <li className="item">
-      <img className="itemImg" src={item.images} alt={item.name}></img>
+      <a href="/">
+        <img className="itemImg" src={item.images} alt={item.name}></img>
+      </a>
       <div className="itemInfo">
-        <h3 className="name">{item.name}</h3>
-        <div className="price">{`${item.price}원`}</div>
+        <a href="/">
+          <h3 className="name">{item.name}</h3>
+        </a>
+        <div className="price">{`${price}원`}</div>
         <div className="favorite">
           <img className="heartImg" src={heart} alt="heart"></img>
           <div className="favoriteCount">{item.favoriteCount}</div>
--- src/BestItems/BestItems.js
+++ src/BestItems/BestItems.js
@@ -2,18 +2,19 @@ import BestItemList from './BestItemList/BestItemList';
 import './BestItems.css';
 
 const style = {
-  listStyle: 'none',
+  height: '1500px',
 };
 
 function BestItems({ items }) {
   return (
     <div className="bestItems">
       <h2>베스트 상품</h2>
-      <ul className="itemList" style={style}>
+      <ul className="itemList">
         {items.map((item, index) => {
           return <BestItemList key={item.id} item={item} index={index} />;
         })}
       </ul>
+      <div style={style}></div> {/* Header position 고정 확인용 */}
     </div>
   );
 }
