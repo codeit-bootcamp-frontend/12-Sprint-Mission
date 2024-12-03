68f5595f86943f8b900f3e6bbedd562c9fbddda0
chanki.kim <chanki.kim89@gmail.com>
2024-12-03 13:26:16 +0900

    feat: ui Button 컴포넌트 color props 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 import clsx from "clsx";
 import styles from "./Button.module.scss";
 
-export function Button({ to, className, children, size, ...props }) {
-  const css = clsx(styles.btn, size && styles[`btn-${size}`], className);
+export function Button({ to, className, children, size, color, ...props }) {
+  const css = clsx(
+    styles.btn,
+    size && styles[`btn-${size}`],
+    color && styles[color],
+    className
+  );
   const Component = to ? Link : "button";
   const ComponentProps = {
     className: css,
