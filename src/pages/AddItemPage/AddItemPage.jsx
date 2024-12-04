8f45f50d8a7dff775db6b3926f5974150ea1e89f
CJewon <112820744+CJewon@users.noreply.github.com>
2024-12-04 10:49:47 +0900

    [최제원] Sprint6 (#245)
    * Revert "[최제원] Sprint4 (#155)"
    
    This reverts commit e46d37a17fb6acf41c63dc0a4c66148acc4f0e9b.
    
    * react npm start 오류로 인한 npm install 및 react 개발환경 셋팅
    
    * header 추가 및 GET 리퀘스트를 받아 itemList 정렬
    
    * 클래스 이름 추가 및 CSS 추가
    
    * 파일 구조 정리 및 상품 정렬 쿼리 생성
    
    * 최신순, 인기순 드롭아웃 html 구현과 검색 input 기능 html 구현
    
    * form 태그 내부에 있는 태그들의 css 추가 및 수정
    
    * 스프린트 미션5 미완성으로 코드잇에서 제공한 스프린트 미션5 완성본을 표본으로 커밋
    
    * addItemPage html과 css 추가
    
    * 이미지 삭제 기능 추가

---
+++ src/pages/AddItemPage/AddItemPage.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./AddItemPage.css";
+import Tag from "./components/Tag";
+function AddItemPage() {
+  const [tags, setTags] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (inputValue.trim()) {
+        setTags((prevTags) => [...prevTags, inputValue.trim()]);
+        setInputValue("");
+      }
+    }
+  };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleDeleteClick = (tags) => {
+    setTags((prevTags) => prevTags.filter((tag) => tag != tags));
+  };
+  return (
+    <div className="addItems__container">
+      <div className="addItems-title__container">
+        <h1 className="addItems-title">상품 등록하기</h1>
+        <button className="addItems-button">등록</button>
+      </div>
+      <form className="input-form__container">
+        <div className="addItems-img__container">
+          <h2 className="addItems-img__title">상품 이미지</h2>
+          <input
+            type="file"
+            placeholder="이미지 등록"
+            className="addItems-img__input"
+            value={value}
+          />
+        </div>
+        <div className="addItems-name__container">
+          <h2 className="addItems-name__title">상품명</h2>
+          <input
+            type="text"
+            placeholder="상품명을 입력해주세요"
+            className="addItems-name__input"
+            value={value}
+          />
+        </div>
+        <div className="addItems-explain__container">
+          <h2 className="addItems-explain__title">상품 소개</h2>
+          <input
+            type="text"
+            placeholder="상품 소개를 입력해주세요"
+            className="addItems-explain__input"
+            value={value}
+          />
+        </div>
+        <div className="addItems-price__container">
+          <h2 className="addItems-price__title">판매가격</h2>
+          <input
+            type="number"
+            className="addItems-price__input"
+            placeholder="판매 가격을 입력해주세요"
+            value={value}
+          />
+        </div>
+        <div className="addItems-tag__container">
+          <h2 className="addItems-tag__title">태그</h2>
+          <div className="addItems-tag__flex-container">
+            <input
+              type="text"
+              placeholder="태그를 입력해주세요"
+              className="addItems-tag__input"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <Tag tags={tags} onDelete={handleDeleteClick}></Tag>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddItemPage;
