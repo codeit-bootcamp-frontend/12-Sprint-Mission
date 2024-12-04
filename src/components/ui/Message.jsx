a30163a605d5e2f3c7f489e2bf7e6252331cce61
chanki.kim <chanki.kim89@gmail.com>
2024-12-04 11:35:28 +0900

    feat: Message 컴포넌트에 compact props추가

--- src/components/ui/Message.jsx
+++ src/components/ui/Message.jsx
@@ -1,8 +1,10 @@
+import clsx from "clsx";
 import styles from "./Message.module.scss";
 
-export function Message({ icon, alt = "", children }) {
+export function Message({ icon, alt = "", compact = false, children }) {
+  const css = clsx(styles.message, compact && styles.compact);
   return (
-    <div className={styles.message}>
+    <div className={css}>
       {icon && <img src={icon} className={styles.icon} alt={alt} />}
       {children}
     </div>
