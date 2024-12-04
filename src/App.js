35996194d5543bdb72105a6b02633fc2e7be6128
songhyungjin <sori4606@naver.com>
2024-12-04 21:24:58 +0900

    fix router issues

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import "./App.css";
 import Header from "./components/layout/Header";
 import BestProducts from "./components/BestProducts/BestProducts";
 import AllProducts from "./components/AllProducts/AllProducts";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function MainContent() {
+  return (
+    <>
+      <BestProducts />
+      <AllProducts />
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <BestProducts />
-        <AllProducts />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/items" element={<MainContent />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
