13d5cf4fcfb7396ffcfd4fda7bc21084148c7374
박형준 <spfe1245@codeit.com>
2024-12-05 14:38:07 +0900

    Add All Items order change

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { getData } from '../api';
 import AllItems from '../Components/AllItems/AllItems';
 import BestItems from '../Components/BestItems/BestItems';
@@ -5,14 +6,30 @@ import Header from '../Components/Header/Header';
 import './App.css';
 
 const { list: bestItems } = await getData('favorite');
-const { list: allItems } = await getData();
+// const { list: allItems } = await getData();
 
 function App() {
+  const [allItems, setAllItems] = useState([]);
+  const [order, setOrder] = useState('recent');
+
+  const handleChange = (orderQuery) => {
+    setOrder(orderQuery);
+  };
+
+  const load = async (order) => {
+    const { list: nextItems } = await getData(order);
+    setAllItems(nextItems);
+  };
+
+  useEffect(() => {
+    load(order);
+  }, [order]);
+
   return (
     <>
       <Header />
       <BestItems items={bestItems} />
-      <AllItems items={allItems} />
+      <AllItems items={allItems} onChange={handleChange} />
     </>
   );
 }
--- src/Components/AllItems/AllItems.js
+++ src/Components/AllItems/AllItems.js
@@ -1,10 +1,29 @@
 import AllItemList from './AllItemList/AllItemList';
 import './AllItems.css';
 
-function AllItems({ items }) {
+function AllItems({ items, onChange }) {
+  const allItemsChange = (e) => {
+    onChange(e.target.value);
+  };
+
   return (
     <div className="allItems">
-      <h2>전체 상품</h2>
+      <div className="container">
+        <h2>전체 상품</h2>
+        <div className="allItemsRight">
+          <input type="text" placeholder="검색할 상품을 입력해주세요"></input>
+          <button>상품 등록하기</button>
+          <select
+            className="orderSelect"
+            name="order"
+            onChange={allItemsChange}
+          >
+            <option value="recent">최신순</option>
+            <option value="favorite">베스트순</option>
+          </select>
+        </div>
+      </div>
+
       <ul className="itemList">
         {items.map((item, index) => {
           return <AllItemList key={item.id} item={item} index={index} />;
