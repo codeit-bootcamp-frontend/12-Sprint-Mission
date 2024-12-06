7e8df83b25ef1886bd33366750e9ef96fb82f96e
Tokyun02 <ehrbsghkrk@naver.com>
2024-12-06 16:55:56 +0900

    chore : tailwind 설정 및 설정에 따른 스타일 일부 수정

--- src/components/layouts/ItemDetailPageLayout/ItemInfo/index.jsx
+++ src/components/layouts/ItemDetailPageLayout/ItemInfo/index.jsx
@@ -1,8 +1,20 @@
 import { useParams } from 'react-router-dom';
+import { getItemDetail } from 'api/productAPI';
+import { useCallback, useEffect, useState } from 'react';
 
 const ItemInfo = () => {
   const { id } = useParams();
-  return <p>{`${id}번 상품`}</p>;
+  const [item, setItem] = useState(null);
+
+  const getItem = useCallback(async () => {
+    const response = await getItemDetail(id);
+    setItem(response);
+  }, [id]);
+
+  useEffect(() => {
+    getItem();
+  }, [getItem]);
+  return <p className="py-4">{JSON.stringify(item)}</p>;
 };
 
 export default ItemInfo;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/components/layouts/ItemDetailPageLayout/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
