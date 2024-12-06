c8eee427abaec61b187d3f0e5b82a0e80b527b14
Tokyun02 <ehrbsghkrk@naver.com>
2024-12-06 21:28:56 +0900

    feat : BackLinkButton 구현

--- src/components/layouts/ItemDetailPageLayout/BackLinkButton/index.jsx
+++ src/components/layouts/ItemDetailPageLayout/BackLinkButton/index.jsx
@@ -1,5 +1,20 @@
+import backArrowImg from 'assets/images/ic_back.svg';
+import { useNavigate } from 'react-router-dom';
+
 const BackLinkButton = () => {
-  return <button>목록으로 돌아가기</button>;
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate('/items');
+  };
+  return (
+    <button
+      className="flex mx-auto py-2 px-6 rounded-full text-white bg-blue-500"
+      onClick={onClick}
+    >
+      <span>목록으로 돌아가기</span>
+      <img src={backArrowImg} alt="아이템 목록 버튼" />
+    </button>
+  );
 };
 
 export default BackLinkButton;
