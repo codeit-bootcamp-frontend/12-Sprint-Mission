7f4621a9b980acb2451e66c38f17dff1dcbff508
LMS10 <ggb7410@naver.com>
2024-12-08 00:05:58 +0900

    feat(component): LikeButton 컴포넌트 구현

---
+++ src/pages/ItemPage/components/LikeButton.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { ReactComponent as HeartSvg } from "../../../assets/images/icons/ic_heart.svg";
+
+const PillButton = styled.button`
+  display: flex;
+  border: 1px solid var(--secondary-200);
+  padding: 0.4rem 1.2rem;
+  border-radius: 3.5rem;
+  align-items: center;
+  gap: 1rem;
+
+  color: var(--secondary-500);
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 2.6rem;
+
+  &:hover svg path {
+    fill: var(--error-red);
+    stroke: var(--error-red);
+  }
+`;
+
+const StyledHeartSVG = styled(HeartSvg)`
+  width: 3.2rem;
+  height: 3.2rem;
+`;
+
+const ButtonContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+`;
+
+function LikeButton({ productId, isFavorite, favoriteCount }) {
+  return (
+    <PillButton>
+      <ButtonContent>
+        <StyledHeartSVG />
+        {favoriteCount.toLocaleString()}
+      </ButtonContent>
+    </PillButton>
+  );
+}
+
+export default LikeButton;
