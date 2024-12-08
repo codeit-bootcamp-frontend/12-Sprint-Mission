a78f85f81ef4d471eb9b8f359e3cad3e88ddbf78
LMS10 <ggb7410@naver.com>
2024-12-08 21:32:55 +0900

    feat(api): 상품 댓글 조회를 위한 API 함수 추가

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -30,3 +30,21 @@ export async function getProductById(productId) {
     throw error;
   }
 }
+
+export async function getProductComments({ productId, params }) {
+  if (!productId) {
+    throw new Error("Invalid product ID");
+  }
+
+  try {
+    const query = new URLSearchParams(params).toString();
+    const response = await fetch(
+      `${BASE_URL}/products/${productId}/comments?${query}`
+    );
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch product comments: ", error);
+    throw error;
+  }
+}
