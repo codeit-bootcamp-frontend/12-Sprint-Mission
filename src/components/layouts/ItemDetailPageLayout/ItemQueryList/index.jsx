b0faf5fed0262cdf9a82ead065d0d2491c845aff
Tokyun02 <ehrbsghkrk@naver.com>
2024-12-08 13:19:11 +0900

    chore : 댓글 없을 시에 대한 조건부 렌더링 수정

--- src/components/layouts/ItemDetailPageLayout/ItemQueryList/index.jsx
+++ src/components/layouts/ItemDetailPageLayout/ItemQueryList/index.jsx
@@ -1,8 +1,18 @@
 import { getItemComments } from 'api/productAPI';
 import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import emptyQueryImg from 'assets/images/Img_inquiry_empty.svg';
 
 import ItemQuery from './ItemQuery';
+
+const EmptyQueryList = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mb-10">
+      <img src={emptyQueryImg} alt="빈 댓글 이미지" />
+      <p className="text-gray-400">아직 문의가 없어요</p>
+    </div>
+  );
+};
 
 const ItemQueryList = () => {
   const { id } = useParams();
@@ -20,13 +30,15 @@ const ItemQueryList = () => {
   if (!comments) {
     return <p>로딩 중</p>;
   }
+  const renderComments =
+    comments.length > 0 ? (
+      comments.map((value) => <ItemQuery key={value.id} query={value} />)
+    ) : (
+      <EmptyQueryList />
+    );
 
   return (
-    <section className="flex flex-col mx-auto w-4/5">
-      {comments.map((value) => (
-        <ItemQuery key={value.id} query={value} />
-      ))}
-    </section>
+    <section className="flex flex-col mx-auto w-4/5">{renderComments}</section>
   );
 };
 
