4b8509b94732afa508acc68c7373242447756443
chanki.kim <chanki.kim89@gmail.com>
2024-12-28 11:15:58 +0900

    Refactor(mentor): module css를 flat 하게 수정

--- src/components/Comment/CommentAdd.jsx
+++ src/components/Comment/CommentAdd.jsx
@@ -1,7 +1,12 @@
 import { CommentForm } from ".";
 import useComment from "./useComment";
+import styles from "./CommentAdd.module.scss";
 
 export function CommentAdd({ name }) {
   const { handleSubmit } = useComment(name);
-  return <CommentForm onCommentSubmit={handleSubmit} />;
+  return (
+    <div className={styles.form}>
+      <CommentForm onCommentSubmit={handleSubmit} />
+    </div>
+  );
 }
--- src/components/Comment/CommentForm.jsx
+++ src/components/Comment/CommentForm.jsx
@@ -1,6 +1,5 @@
 import useForm from "@hooks/useForm";
 import { FieldItem, Form, Textarea } from "@components/Field";
-import { Section } from "@components/Section";
 import { Author, Button } from "@components/ui";
 import { commentSchema } from "./schema";
 import styles from "./CommentForm.module.scss";
@@ -39,48 +38,44 @@ export function CommentForm({
       error={formError}
       onSubmit={handleSubmit(onSubmit)}
     >
-      <Section>
-        <Section.Content>
-          <FieldItem>
-            {!isEdit && (
-              <FieldItem.Label htmlFor="content">문의하기</FieldItem.Label>
-            )}
-            <Textarea
-              size="sm"
-              placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-              {...register("content")}
+      <FieldItem>
+        {!isEdit && (
+          <FieldItem.Label htmlFor="content">문의하기</FieldItem.Label>
+        )}
+        <Textarea
+          size="sm"
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          {...register("content")}
+        />
+      </FieldItem>
+      <div className={styles.footer}>
+        {isEdit ? (
+          <div className={styles.meta}>
+            <Author
+              avatar={initialData.writer.mage}
+              nickname={initialData.writer.nickname}
+              updatedAt={initialData.updatedAt}
             />
-          </FieldItem>
-        </Section.Content>
-        <Section.Footer>
-          {isEdit ? (
-            <div className={styles.meta}>
-              <Author
-                avatar={initialData.writer.mage}
-                nickname={initialData.writer.nickname}
-                updatedAt={initialData.updatedAt}
-              />
-              <div className={styles.controls}>
-                <Button
-                  variant="text"
-                  color="secondary"
-                  size="sm"
-                  onClick={handleClose}
-                >
-                  취소
-                </Button>
-                <Button type="submit" size="sm" disabled={!isFormValid}>
-                  수정 완료
-                </Button>
-              </div>
+            <div className={styles.controls}>
+              <Button
+                variant="text"
+                color="secondary"
+                size="sm"
+                onClick={handleClose}
+              >
+                취소
+              </Button>
+              <Button type="submit" size="sm" disabled={!isFormValid}>
+                수정 완료
+              </Button>
             </div>
-          ) : (
-            <Button type="submit" size="sm" disabled={!isFormValid}>
-              등록
-            </Button>
-          )}
-        </Section.Footer>
-      </Section>
+          </div>
+        ) : (
+          <Button type="submit" size="sm" disabled={!isFormValid}>
+            등록
+          </Button>
+        )}
+      </div>
     </Form>
   );
 }
