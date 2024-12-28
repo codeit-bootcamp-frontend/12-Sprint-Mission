0961d9a400f3811f6540b6a8482ee10a2396c2b8
chanki.kim <chanki.kim89@gmail.com>
2024-12-28 16:24:19 +0900

    Chore: util 폴더내 파일들 ts 작업

--- src/util/formatter.ts
+++ src/util/formatter.ts
@@ -1,9 +1,9 @@
 import dayjs from "dayjs";
 
-export function toWon(price) {
+export function toWon(price: number) {
   return Number(price).toLocaleString() + "원";
 }
 
-export function toDate(date) {
+export function toDate(date: string) {
   return dayjs(date).format("YYYY.MM.DD");
 }
--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,8 +1,10 @@
 import { jwtDecode } from "jwt-decode";
 
-export function isTokenValid(token) {
+export function isTokenValid(token: string) {
   try {
     const { exp } = jwtDecode(token);
+    if (!exp) return false;
+
     return Date.now() < exp * 1000;
   } catch (err) {
     return false;
