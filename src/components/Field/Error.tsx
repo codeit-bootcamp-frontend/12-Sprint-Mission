eca2c5cbea5d4aea12030e91d0cb31e81a128ec3
chanki.kim <chanki.kim89@gmail.com>
2024-12-31 18:19:22 +0900

    Feat: 필드에러컴포넌트 수정

--- src/components/Field/Error.tsx
+++ src/components/Field/Error.tsx
@@ -1,6 +1,9 @@
 import styles from "./Error.module.scss";
 
-export function Error({ error }: { error: string }) {
+export function Error({ error }: { error: string | undefined }) {
+  if (!error) {
+    return null;
+  }
   return (
     <div className={styles["item-error"]}>
       {error || "오류가 발생했습니다."}
