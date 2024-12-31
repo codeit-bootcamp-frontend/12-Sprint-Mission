c865f4d2f9dfa0813db1a4b92807bf9ad5dbd5ec
chanki.kim <chanki.kim89@gmail.com>
2024-12-31 18:18:57 +0900

    Feat: TagsInput 컴포넌트 수정

--- src/components/Field/TagsInput.tsx
+++ src/components/Field/TagsInput.tsx
@@ -1,70 +1,69 @@
-import { KeyboardEvent, useRef } from "react";
+import { forwardRef, KeyboardEvent, useRef } from "react";
 import clsx from "clsx";
 import { Tags } from "@components/ui";
 import { Error } from "@components/Field";
 import styles from "./Input.module.scss";
+import { FieldError } from "react-hook-form";
 
 interface TagsInputProps {
-  error: string | null;
+  error: FieldError | FieldError[] | undefined;
+  isValid: boolean;
+  isTouched: boolean;
+  isDirty: boolean;
   value: string[];
-  id: string;
-  name: string;
-  onChange: (name: string, tags: string[]) => void;
+  onChange: (value: string[]) => void;
   placeholder: string;
 }
 
-export function TagsInput({
-  error,
-  value,
-  id,
-  name,
-  onChange,
-  placeholder,
-}: TagsInputProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const valid = value.length && !error;
-  const css = clsx(
-    styles["field-box"],
-    valid && styles.valid,
-    error && styles.error
-  );
+export const TagsInput = forwardRef<HTMLInputElement, TagsInputProps>(
+  ({ error, value, onChange, isValid, placeholder }: TagsInputProps, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const valid = isValid && !error && value.length;
+    const css = clsx(
+      styles["field-box"],
+      valid && styles.valid,
+      error && styles.error
+    );
 
-  function handleKeyDown(e: KeyboardEvent) {
-    if (e.nativeEvent.isComposing) return;
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.nativeEvent.isComposing) return;
 
-    if (e.key === "Enter") {
-      e.preventDefault();
-      if (!inputRef.current) return;
+      if (e.key === "Enter") {
+        e.preventDefault();
+        if (!inputRef.current) return;
 
-      const tag = inputRef.current.value.trim();
-      if (tag && !value.includes(tag)) {
-        const newTags = [...value, tag];
-        onChange(name, newTags);
+        const tag = inputRef.current.value.trim();
+        if (tag && !value.includes(tag)) {
+          const newTags = [...value, tag];
+          onChange(newTags);
+        }
+        inputRef.current.value = "";
       }
-      inputRef.current.value = "";
     }
-  }
 
-  function handleRemove(tag: string) {
-    const newTags = value.filter((item) => item !== tag);
-    onChange(name, newTags);
-  }
+    function handleRemove(tag: string) {
+      const newTags = value.filter((item) => item !== tag);
+      onChange(newTags);
+    }
 
-  return (
-    <>
-      <div className={styles.field}>
-        <input
-          ref={inputRef}
-          type="text"
-          className={css}
-          onKeyDown={handleKeyDown}
-          id={id}
-          name={name}
-          placeholder={placeholder}
-        />
-      </div>
-      <Tags tags={value} onRemoveItem={handleRemove} />
-      <Error error={error} />
-    </>
-  );
-}
+    const extractError = Array.isArray(error)
+      ? error?.find((err) => err && err.message)?.message ?? null
+      : error?.message;
+
+    return (
+      <>
+        <div className={styles.field}>
+          <input
+            ref={inputRef}
+            type="text"
+            className={css}
+            onKeyDown={handleKeyDown}
+            placeholder={placeholder}
+          />
+        </div>
+        <Tags tags={value} onRemoveItem={handleRemove} />
+        {extractError && <Error error={extractError} />}
+      </>
+    );
+  }
+);
