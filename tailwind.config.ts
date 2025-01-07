47f35cfb58e76ae54b3c9264a7dae68574bc6d67
Tokyun02 <developtk02@gmail.com>
2025-01-07 16:14:32 +0900

    :wrench: chore : Add to tailwind config color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,31 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: '#111827',
+          800: '#1F2937',
+          700: '#374151',
+          600: '#4B5563',
+          500: '#6B7280',
+          400: '#9CA3AF',
+          200: '#E5E7EB',
+          100: '#F3F4F6',
+          50: '#F9FAFB',
+        },
+        blue: {
+          100: '#3692FF',
+          200: '#1967D6',
+          300: '#1251AA',
+        },
+        red: {
+          error: 'F74747',
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
