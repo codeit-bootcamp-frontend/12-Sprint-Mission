e587a373537afec911d7511889118c3f5e98170e
Tokyun02 <developtk02@gmail.com>
2025-01-07 21:57:47 +0900

    :wrench: chore : 이미지 도메인 경로 설정 및 dayjs 의존성 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mblogthumb-phinf.pstatic.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flexible.img.hani.co.kr',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
