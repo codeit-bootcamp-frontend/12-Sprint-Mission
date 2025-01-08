31e41fcbcb98355c36fbaa71d5f2707fea543e89
chanki.kim <chanki.kim89@gmail.com>
2025-01-08 17:06:18 +0900

    feat: middleware 추가

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { auth } from "./app/api/auth/auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    return Response.redirect(new URL("/login", req.nextUrl.origin));
+  }
+});
+
+export const config = {
+  matcher: ["/addBoard"],
+};
