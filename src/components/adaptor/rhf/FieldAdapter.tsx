32d97913f5c0c0358ed86dfab3f8f883fc51dab9
chanki.kim <chanki.kim89@gmail.com>
2025-01-09 00:52:19 +0900

    Refactor: apdator 코드 주석 수정

--- src/components/adaptor/rhf/FieldAdapter.tsx
+++ src/components/adaptor/rhf/FieldAdapter.tsx
@@ -36,10 +36,18 @@ export function FieldAdapter<T extends FieldValues, U extends FieldPath<T>>({
         render({
           ...field,
           onChange: (e) => {
-            // rhf mode가 onBlur모드일때 벨리데이션하려면 필요 (trigger가 onblur에 되는듯함)
-            // 일반적으로 레스트파라미터로 각 input, textarea에 자동적으로 onblur가 들어가서 괜찮으나
-            // 특수하게 커스텀 컴포넌트를 이용해서 수동으로 업데이트할경우(onchange활용하여) onBlur를 해줘야 벨리데이션이 됨
-            // (useForm의 리턴값중에 setValue를 통해서 업데이트하면 벨리데이션이 됨)
+            // 기본 onChange 메서드에 onBlur 메세드를 같이 호출하도록 해두었습니다.
+            //
+            // field.onBlur()는 실제 blur 이벤트가 아닌 validation 트리거용 메서드입니다. (rhf의 mode가 blur일때)
+            //
+            // 일반 controlled input/textarea는
+            // value, onChange, onBlur가 정상적으로 연결이 가능하지만
+            //
+            // 태그인풋/이미지업로드 등의 커스텀 컴포넌트에서
+            // 특정한 행동에 onChange를 활용해 값을 업데이트할때에는
+            // onBlur를 실행하지 못해서, onChange가 호출될때 함께 onBlur가 호출되도록 개선했습니다.
+            //
+            // (정상적으로 연결되는 컴포넌트에서도 한번더 벨리데이션을 호출한다고 해서 문제가 되지 않을것 같아서)
             field.onChange(e);
             field.onBlur();
           },
