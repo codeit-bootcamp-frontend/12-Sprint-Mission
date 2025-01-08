0954260d0626cc558c6ae54033ba6faf070b900c
chanki.kim <chanki.kim89@gmail.com>
2025-01-08 14:05:24 +0900

    feat: landing 페이지 폴더 삭제 (common 그룹의 page로 내용이동)

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,5 +1,17 @@
-import LandingPage from "./_landing/page";
+import Banner from "./_components/Banner";
+import Feature from "./_components/Feature";
+import {
+  heroBannerData,
+  footerBannerData,
+  featureList,
+} from "./_components/landingContents";
 
-export default function Page() {
-  return <LandingPage />;
+export default function LandingPage() {
+  return (
+    <>
+      <Banner {...heroBannerData} />
+      <Feature list={featureList} />
+      <Banner {...footerBannerData} />
+    </>
+  );
 }
