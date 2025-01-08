5a924893703d27eaf24c889c2a8d1194e006504d
chanki.kim <chanki.kim89@gmail.com>
2025-01-08 16:42:21 +0900

    fix: useAsync훅 개선 - 다른컴포넌트에서 무한랜더링 유발 방지용으로 useCallback 적용

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function useAsync<T>(asyncFn: (...args: any[]) => Promise<T>) {
@@ -6,29 +6,32 @@ export default function useAsync<T>(asyncFn: (...args: any[]) => Promise<T>) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  async function wrappedFn(...args: Parameters<typeof asyncFn>) {
-    let isAborted = false;
+  const wrappedFn = useCallback(
+    async function wrappedFn(...args: Parameters<typeof asyncFn>) {
+      let isAborted = false;
 
-    try {
-      setError(null);
-      setIsLoading(true);
-      const res = await asyncFn(...args);
-      setResult(res);
-      return res;
-    } catch (err) {
-      if (err instanceof Error) {
-        if (err.name === "CanceledError") {
-          isAborted = true;
-        } else {
-          setError(err);
+      try {
+        setError(null);
+        setIsLoading(true);
+        const res = await asyncFn(...args);
+        setResult(res);
+        return res;
+      } catch (err) {
+        if (err instanceof Error) {
+          if (err.name === "CanceledError") {
+            isAborted = true;
+          } else {
+            setError(err);
+          }
+        }
+      } finally {
+        if (!isAborted) {
+          setIsLoading(false);
         }
       }
-    } finally {
-      if (!isAborted) {
-        setIsLoading(false);
-      }
-    }
-  }
+    },
+    [asyncFn]
+  );
 
   return { isLoading, setIsLoading, error, result, wrappedFn };
 }
