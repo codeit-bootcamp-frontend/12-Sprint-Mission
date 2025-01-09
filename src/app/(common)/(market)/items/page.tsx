9e6d1a92a9eead4bc8c97ab1ccfdf9b89d523659
chanki.kim <chanki.kim89@gmail.com>
2025-01-09 12:28:16 +0900

    fix: 필요없는 suspense 삭제 (csr)

--- src/app/(common)/(market)/items/page.tsx
+++ src/app/(common)/(market)/items/page.tsx
@@ -1,15 +1,12 @@
 import { PageWrapper } from "@/components/Page";
 import BestItems from "../_components/BestItems";
 import AllItems from "../_components/AllItems";
-import { Suspense } from "react";
 
 export default function ItemsPage() {
   return (
     <PageWrapper>
       <BestItems />
-      <Suspense>
-        <AllItems />
-      </Suspense>
+      <AllItems />
     </PageWrapper>
   );
 }
