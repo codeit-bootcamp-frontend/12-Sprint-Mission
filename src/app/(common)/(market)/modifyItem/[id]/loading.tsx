d53d71ceb64c2d7bafd57a65999ec22581450832
chanki.kim <chanki.kim89@gmail.com>
2025-01-09 16:03:07 +0900

    fix: 상품수정페이지 로딩을 suspense로 변경 - redirect되기전에 로딩문구가 보이는 현상 수정

--- src/app/(common)/(market)/modifyItem/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Message } from "@/components/ui";
-
-export default function loading() {
-  return <Message>상품정보를 가져오는 중입니다...</Message>;
-}
--- src/app/(common)/(market)/modifyItem/[id]/page.tsx
+++ src/app/(common)/(market)/modifyItem/[id]/page.tsx
@@ -3,6 +3,8 @@ import ProductForm from "../../_components/ProductForm";
 import { getProduct } from "@/service/product";
 import { auth } from "@/app/api/auth/auth";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { Message } from "@/components/ui";
 
 export default async function ModifyItemPage({
   params,
@@ -10,7 +12,8 @@ export default async function ModifyItemPage({
   params: Promise<{ id: string }>;
 }) {
   const session = await auth();
-  if (!session) {
+
+  if (!session?.user) {
     redirect("/login");
   }
 
@@ -19,7 +22,9 @@ export default async function ModifyItemPage({
 
   return (
     <PageWrapper>
-      <ProductForm mode="edit" initialData={detail} productId={Number(id)} />
+      <Suspense fallback={<Message>상품정보를 가져오는 중입니다...</Message>}>
+        <ProductForm mode="edit" initialData={detail} productId={Number(id)} />
+      </Suspense>
     </PageWrapper>
   );
 }
