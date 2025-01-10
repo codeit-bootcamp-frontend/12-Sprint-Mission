85ce1d64baa63eb62465896aed43faac4ebdca61
chanki.kim <chanki.kim89@gmail.com>
2025-01-10 11:49:48 +0900

    feat: 자유게시판 상세페이지 에러 처리 추가

--- src/app/(common)/(board)/boards/[id]/page.tsx
+++ src/app/(common)/(board)/boards/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { Temporary } from "@/components/ui";
 import { getArticle } from "@/service/article";
+import axios from "axios";
+import { notFound } from "next/navigation";
 
 export default async function BoardDetailPage({
   params,
@@ -8,12 +10,28 @@ export default async function BoardDetailPage({
 }) {
   const id = (await params).id;
 
-  const detail = await getArticle(Number(id));
+  try {
+    const detail = await getArticle(Number(id));
 
-  return (
-    <Temporary
-      title="게시판 상세"
-      description={`${detail.title}의 상세페이지 : 준비중`}
-    />
-  );
+    if (!detail) {
+      notFound();
+    }
+
+    return (
+      <Temporary
+        title="게시판 상세"
+        description={`${detail.title}의 상세페이지 : 준비중`}
+      />
+    );
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.status === 404) {
+        notFound();
+      }
+
+      throw new Error("게시물 정보를 가져오는데 실패했습니다");
+    }
+
+    throw new Error("알 수 없는 에러가 발생했습니다.");
+  }
 }
