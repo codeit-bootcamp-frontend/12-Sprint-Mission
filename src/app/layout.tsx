ccd91ab86264aeba3b3631b5fb3602e5dcc20d5b
Tokyun02 <developtk02@gmail.com>
2025-01-10 12:53:09 +0900

    :recycle: refactor : SEO 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import localFont from 'next/font/local';
 import './globals.css';
+import { Metadata } from 'next';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -8,6 +9,16 @@ const pretendard = localFont({
   variable: '--font-pretendard',
 });
 
+export const metadata: Metadata = {
+  title: '판다 마켓',
+  description: '판다 마켓에 방문하면 물건을 사고 팔고, 소통을 할 수 있어요!',
+  openGraph: {
+    title: '판다 마켓',
+    description: '판다 마켓에 방문하면 물건을 사고 팔고, 소통을 할 수 있어요!',
+    images: ['/assets/images/home_top_banner.png'],
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko' className={`${pretendard.variable}`}>
+      <head>
+        <link rel='icon' type='image/png' href='/assets/images/logo_img.png' />
+      </head>
       <body className={`${pretendard.className} min-h-screen`}>{children}</body>
     </html>
   );
