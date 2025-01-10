015c33240cc91f9eb495554719f6ec42505beac6
jaehyeongjung <titochung625@naver.com>
2025-01-10 21:55:22 +0900

    chore: prettier-setting

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
-    return (
-        <>
-            <div></div>
-        </>
-    );
+  return (
+    <>
+      <div className="HeaderWrapper">
+        <div className="HeaderContainer">
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
 }
