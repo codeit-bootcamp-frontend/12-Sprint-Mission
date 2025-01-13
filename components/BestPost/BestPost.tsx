be2099ca241d84ee3d96cfda6e5b646f24224112
jaehyeongjung <titochung625@naver.com>
2025-01-13 15:45:34 +0900

    refactor: Post, BestPost Css 규격 맞춤

--- components/BestPost/BestPost.tsx
+++ components/BestPost/BestPost.tsx
@@ -7,7 +7,21 @@ export default function BestPost() {
                 <div className={styles.BestPost}>
                     <div className={styles.BestPostTitle}>베스트 게시글</div>
                     <div className={styles.BestPostTopic}>
-                        <div className={styles.BestPostTopicmap}></div>
+                        <div className={styles.BestPostTopicMap}>
+                            <div className={styles.BestTopicImg}></div>
+                            <div className={styles.BestTopicContent}>
+                                <div
+                                    className={styles.BestTopicContentText}
+                                ></div>
+                                <img
+                                    className={styles.BestTopicContentImg}
+                                ></img>
+                            </div>
+                            <div className={styles.BestTopicDetail}>
+                                <div className={styles.BestTopicDetailData}></div>
+                                <div className={styles.BestTopicDetailDate}></div>
+                            </div>
+                        </div>
                         <div className={styles.BestPostTopicmap}></div>
                         <div className={styles.BestPostTopicmap}></div>
                     </div>
--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -11,9 +11,17 @@ export default function Post() {
                     </button>
                 </div>
                 <div className={styles.PostComment}>
-                    <div className={styles.PostCommentBar}></div>
+                    <div className={styles.PostCommentBar}>
+                        <input className={styles.PostCommentBarInput}></input>
+                        <div className={styles.PostCommentBarDropdown}></div>
+                    </div>
                     <div className={styles.PostCommentSection}>
-                        <div className={styles.PostCommentMap}></div>
+                        <div className={styles.PostCommentMap}>
+                            <div className={styles.PostComment}>
+                                <div className={styles.PostCommentData}></div>
+                                <div className={styles.PostCommentDetail}></div>
+                            </div>
+                        </div>
                         <div className={styles.PostCommentMap}></div>
                     </div>
                 </div>
