cc43d1b8a496918c543a7c666b2cd2c39b8845fc
jaehyeongjung <titochung625@naver.com>
2025-01-13 22:00:34 +0900

    feat: api- Post data 불러오기

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -2,8 +2,25 @@ import styles from "./Post.module.css";
 import profile from "@/public/profile.svg";
 import Image from "next/image";
 import heart from "@/public/heart.svg";
+import { useEffect, useState } from "react";
 
 export default function Post() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        async function fetchArticles() {
+            const response = await fetch(
+                "/api/articles?page=1&pageSize=5&orderBy=recent"
+            );
+            const data = await response.json();
+            if (data.list) {
+                setArticles(data.list);
+            }
+        }
+
+        fetchArticles();
+    }, []);
+
     return (
         <>
             <div className={styles.Post}>
@@ -19,82 +36,95 @@ export default function Post() {
                         <div className={styles.PostCommentBarDropdown}></div>
                     </div>
                     <div className={styles.PostCommentSection}>
-                        <div className={styles.PostCommentMap}>
-                            <div className={styles.PostComment}>
-                                <div className={styles.PostCommentData}>
-                                    <div className={styles.PostCommentDataText}>
-                                        맥북 16인치 16기가 1테라 정도 사양이면
-                                        얼마에 팔아야하나요?
-                                    </div>
-                                    <div
-                                        className={styles.PostCommentDataImg}
-                                    ></div>
-                                </div>
-                                <div className={styles.PostCommentDetail}>
-                                    <div
-                                        className={styles.PostCommentDetailData}
-                                    >
+                        {articles.map((article) => (
+                            <div className={styles.PostCommentMap}>
+                                <div className={styles.PostComment}>
+                                    <div className={styles.PostCommentData}>
                                         <div
                                             className={
-                                                styles.PostCommentDetailDataImg
+                                                styles.PostCommentDataText
                                             }
                                         >
-                                            <Image
-                                                src={profile}
-                                                alt="profile"
-                                                width={24}
-                                                height={24}
-                                            />
+                                            {article.content}
                                         </div>
                                         <div
                                             className={
-                                                styles.PostCommentDetailDataContainer
+                                                styles.PostCommentDataImg
+                                            }
+                                        >
+                                            <img src={article.image}></img>
+                                        </div>
+                                    </div>
+                                    <div className={styles.PostCommentDetail}>
+                                        <div
+                                            className={
+                                                styles.PostCommentDetailData
                                             }
                                         >
                                             <div
                                                 className={
-                                                    styles.PostCommentDetailDataNickname
+                                                    styles.PostCommentDetailDataImg
                                                 }
                                             >
-                                                총명한 판다
+                                                <Image
+                                                    src={profile}
+                                                    alt="profile"
+                                                    width={24}
+                                                    height={24}
+                                                />
                                             </div>
                                             <div
                                                 className={
-                                                    styles.PostCommentDetailDataDate
+                                                    styles.PostCommentDetailDataContainer
                                                 }
                                             >
-                                                2024. 04. 16
+                                                <div
+                                                    className={
+                                                        styles.PostCommentDetailDataNickname
+                                                    }
+                                                >
+                                                    {article.writer.nickname}
+                                                </div>
+                                                <div
+                                                    className={
+                                                        styles.PostCommentDetailDataDate
+                                                    }
+                                                >
+                                                    {new Date(
+                                                        article.createdAt
+                                                    ).toLocaleDateString()}
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div
-                                        className={
-                                            styles.PostCommentDetailFavorite
-                                        }
-                                    >
                                         <div
                                             className={
-                                                styles.PostCommentDetailFavoriteImg
+                                                styles.PostCommentDetailFavorite
                                             }
                                         >
-                                            <Image
-                                                src={heart}
-                                                alt="heart"
-                                                width={24}
-                                                height={24}
-                                            />
-                                        </div>
-                                        <div
-                                            className={
-                                                styles.PostCommentDetailFavoriteNum
-                                            }
-                                        >
-                                            9999+
+                                            <div
+                                                className={
+                                                    styles.PostCommentDetailFavoriteImg
+                                                }
+                                            >
+                                                <Image
+                                                    src={heart}
+                                                    alt="heart"
+                                                    width={24}
+                                                    height={24}
+                                                />
+                                            </div>
+                                            <div
+                                                className={
+                                                    styles.PostCommentDetailFavoriteNum
+                                                }
+                                            >
+                                                {article.likeCount}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
