55694327cf4ab442a87cc033750583e3e420eaab
Namgyeon <getam101@naver.com>
2025-01-20 14:28:45 +0900

    feat: favicon추가, font:Pretendard 적용

--- next/src/pages/boards/index.tsx
+++ next/src/pages/boards/index.tsx
@@ -2,10 +2,14 @@ import styles from "./index.module.css";
 import MainHeader from "@/src/components/MainHeader";
 import BestPosts from "./components/BestPosts";
 import AllPosts from "./components/AllPosts";
+import Head from "next/head";
 
 export default function Page() {
   return (
     <>
+      <Head>
+        <title>판다마켓-게시글</title>
+      </Head>
       <MainHeader />
       <div className={styles.container}>
         <BestPosts />
