a2eab7c6b0f0a87af6c144adc1a29fb5cb0f4859
이승환 <ls991217@naver.com>
2025-01-20 17:59:23 +0900

    feat: borads페이지 임의 생성

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./components/App";
 import ItemsPage from "./pages/ItemsPage";
 import AddItem from "./pages/AddItem";
+import BoardsPage from "./pages/BoardsPage";
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/items" element={<ItemsPage />} />
+          <Route path="/boards" element={<BoardsPage />} />
           <Route path="/additem" element={<AddItem />} />
         </Route>
       </Routes>
---
+++ src/pages/BoardsPage.js
@@ -0,0 +1,5 @@
+function BoardsPage() {
+  return <div>준비중</div>;
+}
+
+export default BoardsPage;
