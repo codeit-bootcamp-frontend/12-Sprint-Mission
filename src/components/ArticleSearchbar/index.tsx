8e165171680f2565cf70cf767e10df7c09d19853
Tokyun02 <developtk02@gmail.com>
2025-01-20 11:45:37 +0900

    :recycle: refactor : 검색 아이콘 크기 반응형 개선

--- src/components/ArticleSearchbar/index.tsx
+++ src/components/ArticleSearchbar/index.tsx
@@ -31,11 +31,19 @@ export default function Searchbar() {
   };
 
   return (
-    <div className='flex-1 flex items-center gap-2 rounded-xl px-4 py-2 bg-gray-100'>
+    <div className='flex-1 flex items-center gap-2 rounded-xl pl-4 py-2 bg-gray-100'>
       <label htmlFor='article-search'>
         <Image src='/assets/icons/search.svg' alt='검색 아이콘' width={16} height={16} style={{ width: 16, height: 16 }} />
       </label>
-      <input type='text' id='article-search' placeholder='검색할 상품을 입력해주세요' className='flex-1 bg-gray-100 outline-none' value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
+      <input
+        type='text'
+        id='article-search'
+        placeholder='검색할 상품을 입력해주세요'
+        className='flex-1 bg-gray-100 text-sm outline-none md:text-base'
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
     </div>
   );
 }
