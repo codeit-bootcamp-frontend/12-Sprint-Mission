ce50d73135d390bd1e5f8a615a2e21abca020d2c
nagyum <wildanimal19@naver.com>
2025-01-23 18:30:40 +0900

    상품 상세 페이지 UI 수정

--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ItemPageDescription from "./components/ItemPageDescription";
 import style from "./ItemPage.module.scss";
 import ItemComment from "./components/ItemComment";
 import Comments from "./components/Comments";
+import backBtn from "../../img/ic_back.svg";
 
 function Itempage() {
   const { id } = useParams(); // URL에서 id 파라미터를 추출
   return (
     <div className={style.container}>
       <ItemPageDescription id={id} />
-      <hr />
+      <hr
+        style={{
+          marginTop: "40px",
+          border: "1px solid,rgba(229, 231, 235, 1) ",
+        }}
+      />
       <ItemComment />
       <Comments id={id} />
+      <Link className={style.link} to="/items">
+        목록으로 돌아가기
+        <img
+          style={{ width: "19px", height: "26px" }}
+          src={backBtn}
+          alt="뒤로 가기"
+        />
+      </Link>
     </div>
   );
 }
--- src/components/ItemPage/components/ItemComment.jsx
+++ src/components/ItemPage/components/ItemComment.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
+
 export default function ItemComment() {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <>
-      <div>문의하기</div>
-      <input
-        placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-        style={{ height: "72px", width: "1152px", marginTop: "8px" }}
+    <div
+      style={{
+        position: "relative",
+        width: "1200px",
+        height: "104px",
+        margin: "16px 0px",
+      }}
+    >
+      <textarea
+        placeholder="  개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시
+        모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게
+        있습니다."
+        style={{
+          height: "104px",
+          width: "100%",
+          fontWeight: "400",
+          fontSize: "16px",
+          lineHeight: "26px",
+          border: "1px solid #D1D5DB",
+          borderRadius: "12px",
+          resize: "none",
+          backgroundColor: "#F3F4F6",
+          paddingTop: "10px",
+        }}
+        value={inputValue}
+        onChange={handleInputChange}
       />
-      <button>등록</button>
-    </>
+      <button
+        style={{
+          position: "absolute",
+          bottom: "-60px",
+          right: "8px",
+          padding: "10px 20px",
+          backgroundColor: inputValue.trim() ? "#007BFF" : "#9CA3AF",
+          color: "#fff",
+          border: "none",
+          borderRadius: "8px",
+          cursor: inputValue.trim() ? "pointer" : "not-allowed",
+        }}
+        disabled={!inputValue.trim()}
+      >
+        등록
+      </button>
+    </div>
   );
 }
