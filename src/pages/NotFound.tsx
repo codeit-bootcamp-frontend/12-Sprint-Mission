cb7a13516def7ec7a5acaf9966afd5f9ec95600b
parkingun <engi9364@naver.com>
2025-01-24 16:33:22 +0900

    feat: 404에러 페이지 추가

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+// pages/NotFound.jsx
+import React from "react";
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  );
+}
+
+export default NotFound;
