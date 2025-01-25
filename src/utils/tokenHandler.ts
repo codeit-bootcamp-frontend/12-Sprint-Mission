8a5a0683d212b86036ec7a86f73c379aa9f1743a
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-01-25 13:29:03 +0900

    refactor: tokenHandler.ts 상수값 대문자로 변경

--- src/utils/tokenHandler.ts
+++ src/utils/tokenHandler.ts
@@ -1,5 +1,5 @@
-const ACCESS_TOKEN_KEY = "accessToken";
-const REFRESH_TOKEN_KEY = "refreshToken";
+const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY" as const;
+const REFRESH_TOKEN_KEY = "REFRESH_TOKEN_KEY" as const;
 
 export function saveTokens(accessToken: string, refreshToken: string) {
   localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
