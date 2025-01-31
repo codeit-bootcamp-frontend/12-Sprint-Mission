eac2cb40abbb0b04f2ff41bcbee4ee85c21c92bb
chanki.kim <chanki.kim89@gmail.com>
2025-01-31 11:23:07 +0900

    refactor(mentro): 내정보 페이지 이동을 link태그로 수정

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -2,16 +2,13 @@
 
 import { Dropdown, Avatar } from "@components/ui";
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
+import Link from "next/link";
 interface ProfileProps {
   nickname: string;
   image: string;
 }
 
 export function Profile({ nickname, image }: ProfileProps) {
-  const router = useRouter();
-
   function handleLogout() {
     if (confirm("정말로 로그아웃 하시겠습니까?")) {
       signOut();
@@ -24,8 +21,8 @@ export function Profile({ nickname, image }: ProfileProps) {
         <Avatar nickname={nickname} img={image} hover />
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        <Dropdown.Item onClick={() => router.push("/mypage")}>
-          내정보
+        <Dropdown.Item asChild>
+          <Link href="/mypage">내정보</Link>
         </Dropdown.Item>
         <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
       </Dropdown.Menu>
