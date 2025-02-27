86abc25010f9083fdde9db0635a657200e5c4abb
chanki.kim <chanki.kim89@gmail.com>
2025-02-27 11:34:25 +0900

    fix: 내정보페이지 강제 ssr 지정

--- src/app/(common)/(user)/editProfile/page.tsx
+++ src/app/(common)/(user)/editProfile/page.tsx
@@ -3,10 +3,13 @@ import { getQueryClient } from "@/util/getQueryClient";
 import { getUserOptions } from "@/service/user.queries";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default async function EditProfilePage() {
+// build시에 ssg로 빌드되는것을 막기위해서
+export const dynamic = "force-dynamic";
+
+export default function EditProfilePage() {
   const queryClient = getQueryClient();
 
-  queryClient.prefetchQuery(getUserOptions);
+  void queryClient.prefetchQuery(getUserOptions);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
--- src/app/(common)/(user)/mypage/page.tsx
+++ src/app/(common)/(user)/mypage/page.tsx
@@ -4,6 +4,9 @@ import { getQueryClient } from "@/util/getQueryClient";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getUserActivityOptions, getUserOptions } from "@/service/user.queries";
 
+// build시에 ssg로 빌드되는것을 막기위해서
+export const dynamic = "force-dynamic";
+
 export default async function UserPage() {
   const queryClient = getQueryClient();
 
